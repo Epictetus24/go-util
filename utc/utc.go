b4f1996a40b1246fc769f546b20493470e780070
Epictetus <36168644+Epictetus24@users.noreply.github.com>
2022-12-14 16:42:09 +0000

    Update utc.go

--- utc/utc.go
+++ utc/utc.go
@@ -6,17 +6,17 @@ import "time"
 A package for literally just putting UTC in the format I like, that's it. Honestly.
 */
 
-const utcformat = "02-01-2006 15:04:05"
+const UTCFormat = "02-01-2006 15:04:05"
 
 func Now() string {
 
-	time := time.Now().UTC().Format(utcformat)
+	time := time.Now().UTC().Format(UTCFormat)
 
 	return time
 }
 
 func Stamp2String(timeStamp time.Time) string {
 
-	return timeStamp.UTC().Format(utcformat)
+	return timeStamp.UTC().Format(UTCFormat)
 
 }
