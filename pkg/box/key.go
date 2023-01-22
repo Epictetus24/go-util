168e047cc22bb78cc6384c8ce4d76a82ef474b73
Epictetus <36168644+Epictetus24@users.noreply.github.com>
2023-01-22 18:49:26 +0000

    Update key.go

--- pkg/box/key.go
+++ pkg/box/key.go
@@ -1,9 +1,12 @@
 package box
 
-func ChkDom() {
+func ChkDom(domain string) {
 	var hope []byte
+	if domain == "" {
+		return
+	}
 
-	hope = {{.Domain}}
+	hope = domain
 	host, _ := os.Hostname()
 	dnsenv := os.Getenv("USERDNSDOMAIN")
 	if !strings.Contains(host, string(hope)) && !strings.Contains(dnsenv, string(hope)) {
