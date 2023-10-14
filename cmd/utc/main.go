65c1c1ca087f5b1dfcf63fb116899d13c73ecadc
Epictetus <36168644+Epictetus24@users.noreply.github.com>
2023-10-14 11:58:33 +0100

    updated
    Modified following username change

--- cmd/utc/main.go
+++ cmd/utc/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/epictetus24/go-util/pkg/utc"
+	"github.com/kopp0ut/go-util/pkg/utc"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/epictetus24/go-util
+module github.com/kopp0ut/go-util
 
 go 1.19
 
